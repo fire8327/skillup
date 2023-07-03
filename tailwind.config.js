0a25b17988ad870f67fcdfc6ce39f0a1df9cac0d
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-07-03 04:16:05 +0300

    upd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,10 @@
-/* import FormKitVariants from '@formkit/themes/tailwindcss' */
 export default {
-    /* darkMode:'class',  
-    content:[
-      './node_modules/@formkit/themes/dist/tailwindcss/genesis/index.cjs'
-    ],   */
     theme: {
       fontFamily: {
           'Inter': 'Inter'
       }
-    },/*  
+    }, 
     plugins: [
       FormKitVariants
-    ], */
+    ],
   }
